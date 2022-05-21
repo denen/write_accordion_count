ae1403f8fbd567bfbdc5423a2b3b6ef35c34f7d6
rbpchan <rbpng221@yahoo.co.jp>
2022-05-21 15:18:18 +0900

    初期テンプレ表示／右クリックh2h3の修正

--- js/main.js
+++ js/main.js
@@ -6,22 +6,28 @@
 
 var new_h2_block = "<div><h2>追加した要素です</h2></div>"
 
-function h2_h3_change() { //親要素の.lavel-boxを取得。子要素を直接操作でタグを変更するとフォーカス？？が外れてしまう。またgetしないといけない
-    console.log("引数として渡されました");
-    console.log(target_div);
-    nakami = target_div.querySelector(".h2").innerHTML;
+function h2_h3_change(e) { //親要素の.lavel-boxを取得。子要素を直接操作でタグを変更するとフォーカス？？が外れてしまう。またgetしないといけない
+    //console.log("引数として渡されました"); //単純にグローバル変数に渡されただけでは・・・？
+    //console.log(target_div);
+    console.log("selected_labelの表示")
+    console.log(selected_label);
+    nakami = selected_label.querySelector(".h2").innerHTML;
     console.log(nakami);
-    tag = target_div.querySelector(".h2").tagName;
+    tag = selected_label.querySelector(".h2").tagName;
     if (tag == "H2") {
-        target_div.querySelector(".h2").outerHTML = "<h3 class='h2'></h3>";
-        target_div.querySelector(".h2").innerHTML = nakami;
+        selected_label.querySelector(".h2").outerHTML = "<h3 class='h2'></h3>";
+        selected_label.querySelector(".h2").innerHTML = nakami;
     } else if (tag == "H3") {
-        target_div.querySelector(".h2").outerHTML = "<h2 class='h2'></h2>";
-        target_div.querySelector(".h2").innerHTML = nakami;
+        selected_label.querySelector(".h2").outerHTML = "<h2 class='h2'></h2>";
+        selected_label.querySelector(".h2").innerHTML = nakami;
     }
+    document.getElementById('contextmenu').style.display = "none";
+    //e.target.removeEventListener("dblclick", h2_h3_change); //リムーブしなくてもなぜか消えてる？？？意味不明・・・
+
 }
 
-function add_h2(event) {
+function add_h2(event) { //　↓初期テンプレート表示と合体させたい！！
+    //
     var count = document.getElementsByClassName("label-box").length;
     var t = event.target;
     var template = document.getElementById("h2"); //document.queryselectorでもいい https://zenn.dev/harryduck/articles/e3e6c9d37e0169c05096
@@ -38,13 +44,12 @@ function add_h2(event) {
 
     setTimeout(() => { label_box.style.backgroundColor = 'aqua' }, 1500);
 
-
-    new_add_div = document.getElementById(input.id).parentElement.querySelector(".add-contener");
-    new_add_div.addEventListener('click', add_h2);
-    new_div_text = document.getElementById(input.id).parentElement.querySelector(".textarea");
-    new_div_text.addEventListener('keyup', keyup_event);
-    new_label_input = document.getElementById(input.id).parentElement.querySelector(".h2");
-    new_label_input.addEventListener('dblclick', dblclick_label);
+    contener = document.getElementById(input.id).parentElement
+        //イベントリスナー追加
+    contener.querySelector(".add-contener").addEventListener('click', add_h2);
+    contener.querySelector(".textarea").addEventListener('keyup', keyup_event);
+    contener.querySelector(".h2").addEventListener('dblclick', dblclick_label);
+    contener.querySelector(".label-box").addEventListener('contextmenu', menu);
 }
 
 function keyup_event(event) {
@@ -107,38 +112,62 @@ const all_moji = document.getElementById("all_moji");
 const add_contener_div = document.getElementsByClassName("add-contener")
     //const contener = document.getElementsByClassName("contener");
     //document.addEventListener('click', click_input);
-new_label_input = document.querySelector(".h2");
-new_label_input.addEventListener('dblclick', dblclick_label);
+
 
 
 //for (i = 0; i < labels.length; i++) {
 //    labels[i].addEventListener('dblclick', dblclick_label);
 //}
 
-for (i = 0; i < add_contener_div.length; i++) {
-    add_contener_div[i].addEventListener('click', add_h2);
-}
+//for (i = 0; i < add_contener_div.length; i++) {
+//    add_contener_div[i].addEventListener('click', add_h2);
+//}
 
-for (i = 0; i < texts.length; i++) {
-    texts[i].addEventListener('keyup', keyup_event);
-}
+//for (i = 0; i < texts.length; i++) {
+//    texts[i].addEventListener('keyup', keyup_event);
+//}
 
 //https://blog-and-destroy.com/24410
 
+let selected_label
+
+function menu(e) {
+    selected_label = e.currentTarget
+    console.log(selected_label);
+    document.getElementById('contextmenu').style.left = e.pageX + "px";
+    document.getElementById('contextmenu').style.top = e.pageY + "px";
+    document.getElementById('contextmenu').style.display = "block";
+    document.getElementById('contextmenu').querySelector("li").addEventListener("click", h2_h3_change);
+}
+
+
+//テンプレートをロード
+window.onload = function() {
+    var template = document.getElementById("h2"); //document.queryselectorでもいい https://zenn.dev/harryduck/articles/e3e6c9d37e0169c05096
+    var content = template.content; //　templateを取得してから中身を取得しないととれない　https://code-kitchen.dev/html/template/
+    var clone = content.cloneNode(true); //importnodeは他のドキュメントに属するノードをクローンする  https://yossan.hatenablog.com/entry/2020/03/29/185739
+    //clonenodeでも良い　https://developer.mozilla.org/ja/docs/Web/HTML/Element/template
+    clone.querySelector("#count1").id = 1;
+    clone.querySelector(".label").setAttribute("for", 1);
+    document.getElementById("title").insertBefore(clone, null); //要素追加
+    document.querySelector(".h2").addEventListener('dblclick', dblclick_label);
+    document.querySelector(".add-contener").addEventListener('click', add_h2);
+    document.querySelector(".label-box").addEventListener('contextmenu', menu);
+}
+
 //https://www.sejuku.net/blog/92015
 //右クリックメニュー
-window.onload = function() {
-    document.body.addEventListener('contextmenu', function(e) {
-        console.log(e); //pointerEvent
-        target_div = e.path[0];
-        console.log(target_div); //pointerEvetのpath[0]
+//document.querySelector(".label").addEventListener('contextmenu', function(e) {
+//    console.log(e); //pointerEvent
+//    selected_label = e.path[0];
+//    console.log(selected_label); //pointerEvetのpath[0]
 
-        document.getElementById('contextmenu').style.left = e.pageX + "px";
-        document.getElementById('contextmenu').style.top = e.pageY + "px";
-        document.getElementById('contextmenu').style.display = "block";
-        document.getElementById('contextmenu').querySelector("li").addEventListener("dblclick", { target_div: target_div, handleEvent: h2_h3_change });
-    });
-}
+//    document.getElementById('contextmenu').style.left = e.pageX + "px";
+//    document.getElementById('contextmenu').style.top = e.pageY + "px";
+//    document.getElementById('contextmenu').style.display = "block";
+//    document.getElementById('contextmenu').querySelector("li").addEventListener("dblclick", { selected_label: selected_label, handleEvent: h2_h3_change });
+//});
+
 
 function menu1(e) {
     alert(e);
